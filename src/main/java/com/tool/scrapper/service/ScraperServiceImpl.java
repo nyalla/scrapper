0c5277e8b664f9efaf038ebd0f808e1bf49e9155
@nyalla <naveenyalla22@gmail.com>
2022-06-30 17:08:56 +0530

    feeder names

--- src/main/java/com/tool/scrapper/service/ScraperServiceImpl.java
+++ src/main/java/com/tool/scrapper/service/ScraperServiceImpl.java
@@ -146,9 +146,9 @@ public class ScraperServiceImpl {
     private void postAlertToTelegram(Set<PowerDTO> responseDTOS, String outageArea) throws IOException {
         //https://api.telegram.org/bot5576678061:AAH_bsRWn_EFqvUi8ZM1GrM-EJ0QtaNdIuE/sendMessage?chat_id=-1001540845770&text=by_bot
 
-        String alertData = responseDTOS.stream().filter(i -> i.getFeederName().contains(outageArea == null && outageArea.isEmpty() ? "TOURIST" : outageArea)).map(i -> i.getFeederName() + ": Power outage from " + i.getFrom() + " to " + i.getTo() + " due to " + i.getType()).collect(Collectors.joining(","));
+        String alertData = responseDTOS.stream().filter(i -> i.getFeederName().contains(outageArea == null && outageArea.isEmpty() ? "TOURIST" : outageArea.toUpperCase())).map(i -> i.getFeederName() + ": Power outage from " + i.getFrom() + " to " + i.getTo() + " due to " + i.getType()).collect(Collectors.joining(","));
         if (alertData == null || alertData.isEmpty())
-            alertData = "No outage in given Area";
+            alertData = "No outage in given "+outageArea+" Area";
         OkHttpClient client = getUnsafeOkHttpClient();
         MediaType mediaType = MediaType.parse("text/plain");
         RequestBody body = RequestBody.create(mediaType, "");
